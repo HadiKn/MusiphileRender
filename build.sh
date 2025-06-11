9ddabac2348a265184679384aa25218268c5458c
HadiKn <hadi.padihasi@gmail.com>
2025-06-11 10:15:38 -0800

    creating super user

--- build.sh
+++ build.sh
@@ -2,8 +2,14 @@
 # Exit on error
 set -o errexit
 
-# Apply any outstanding database migrations
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Apply database migrations
 python manage.py migrate --no-input
 
 # Collect static files
 python manage.py collectstatic --no-input --clear
+
+# Create superuser if it doesn't exist
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists() or User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
